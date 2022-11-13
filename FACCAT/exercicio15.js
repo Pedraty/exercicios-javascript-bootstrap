8020f217fa95386bfbeae710e051348c32bccb28
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-11-13 16:41:32 -0300

    Positivo ou Negativo
    Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).

---
+++ FACCAT/exercicio15.js
@@ -0,0 +1,9 @@
+function teste(){;
+    var numero = parseFloat(prompt("Digite um número"));
+    if (numero<0){;
+        alert("O número digitado é negativo");
+
+    }else {;
+        alert("O número digitado é positivo");
+    };
+};
