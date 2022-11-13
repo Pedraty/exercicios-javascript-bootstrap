204ed5939332cbba2ce446964ad534fa410162cd
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-11-13 16:42:27 -0300

    Compra de Maçãs
    As  maçãs  custam  R$  1,30  cada  se  forem  compradas  menos  de  uma  dúzia, e  R$  1,00  se  forem compradas  pelo  menos  12.  Escreva  um  programa  que  leia  o  número  de  maçãs  compradas,  calcule  e escreva o custo total da compra.

---
+++ FACCAT/exercicio16.js
@@ -0,0 +1,11 @@
+function teste(){;
+    var macas = parseFloat(prompt("Digite um número"));
+    if (macas<12){;
+        var total=macas*1.30
+        alert(macas+" maçãs = "+total+" R$");
+
+    }else {;
+        var total=macas
+        alert(macas+" maçãs = "+total+" R$");
+    };
+};
