a5d4b32d2fca7bd9fee45d730cb1c378b6c7c325
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-11-13 16:17:41 -0300

    Delete exercicio04.js

--- exercicio04.js
+++
@@ -1,7 +0,0 @@
-function subtracao(){
-    var numero1, numero2, resultado;
-    numero1=parseFloat(prompt("Digite um valor"));
-    numero2=parseFloat(prompt("Digite um valor"));
-    resultado=numero1-numero2;
-    alert(numero1+" - "+numero2+" = "+resultado)
-}
