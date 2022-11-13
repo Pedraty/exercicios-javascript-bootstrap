13de30714d4ced2493d4d04542c89c5ee42dd91f
Pedro Henrique Fonseca <107948594+Pedraty@users.noreply.github.com>
2022-11-13 16:15:18 -0300

    Soma
    Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a soma dos valores.

---
+++ lista01/exercicio03.js
@@ -0,0 +1,7 @@
+function soma(){
+    var numero1, numero2, resultado;
+    numero1=parseFloat(prompt("Digite uma parcela"));
+    numero2=parseFloat(prompt("Digite outra parcela"));
+    resultado = numero1+numero2
+    alert(numero1+" + "+numero2+" = "+resultado)
+}
